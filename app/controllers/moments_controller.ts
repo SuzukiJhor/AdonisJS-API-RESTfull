63fa40b3aeecb4302838d609b1bbff4d63fb5e4e
JhordanS <jhordan.kendy1@gmail.com>
2024-04-24 22:02:38 -0300

    feat: define method store

--- app/controllers/moments_controller.ts
+++ app/controllers/moments_controller.ts
@@ -1,9 +1,15 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import Moment from '#models/moment'
 
 export default class MomentsController {
-  async store() {
+  async store({ request, response }: HttpContext) {
+    const body = request.body()
+    const moment = await Moment.create(body)
+
+    response.status(201)
     return {
-      msg: 'deu certo!',
+      message: 'Momento criado com sucesso',
+      data: moment,
     }
   }
 }
