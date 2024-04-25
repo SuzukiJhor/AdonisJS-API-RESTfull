008e6201a7b7565995984b0d64e125fbc7fb1982
JhordanS <jhordan.kendy1@gmail.com>
2024-04-25 00:29:29 -0300

    feat: create method update

--- app/controllers/moments_controller.ts
+++ app/controllers/moments_controller.ts
@@ -60,4 +60,32 @@ export default class MomentsController {
       }
     }
   }
+
+  async update({ params, request }: HttpContext) {
+    const body = request.body()
+    const moment = await Moment.findOrFail(params.id)
+
+    moment.title = body.title
+    moment.description = body.description
+
+    if (moment.image !== body.image || !moment.image) {
+      const image = request.file('image', this.validationOptions)
+
+      if (image) {
+        const imageName = `${uuidv4()}.${image.extname}`
+        await image.move(Application.tmpPath('uploads'), {
+          name: imageName,
+        })
+
+        moment.image = imageName
+      }
+    }
+
+    await moment.save()
+
+    return {
+      message: 'Momento atualizado com sucesso!',
+      data: moment,
+    }
+  }
 }
