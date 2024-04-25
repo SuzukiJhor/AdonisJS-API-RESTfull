68c35dde9570c28523d31adbfc9b9fdad369eb58
JhordanS <jhordan.kendy1@gmail.com>
2024-04-25 00:42:52 -0300

    feat: create model of comments

--- app/models/coment.ts
+++ app/models/coment.ts
@@ -1,20 +1,15 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
-import Coment from './coment'
 
-export default class Moment extends BaseModel {
-  @hasMany(() => Coment)
+export default class Coment extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
-  @column()
-  declare title: string
+  @column
+  declare username: string
 
-  @column()
-  declare description: string
-
-  @column()
-  declare image: string
+  @column
+  declare text: string
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
@@ -22,4 +17,3 @@ export default class Moment extends BaseModel {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
 }
-99
