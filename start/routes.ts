c39f38b74bf1da27735277c02fc7ac2e78be1ac0
JhordanS <jhordan.kendy1@gmail.com>
2024-04-26 16:52:01 -0300

    feat: create routes to Coments

--- start/routes.ts
+++ start/routes.ts
@@ -8,10 +8,14 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const CommentsController = () => import('#controllers/comments_controller')
 const MomentsController = () => import('#controllers/moments_controller')
 
 router
   .group(() => {
     router.resource('/moments', MomentsController).apiOnly()
+
+    router.post('/moments/:momentId/comments', [CommentsController, 'store'])
+    router.get('/moments/:momentId/comments', [CommentsController, 'show'])
   })
   .prefix('/api')
