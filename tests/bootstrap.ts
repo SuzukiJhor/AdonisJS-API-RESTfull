93dbcfcefdc2689d350ad1090b9df4a539e4e6ce
JhordanS <jhordan.kendy1@gmail.com>
2024-04-26 21:00:41 -0300

    feat: create test

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -2,37 +2,19 @@ import { assert } from '@japa/assert'
 import { apiClient } from '@japa/api-client'
 import app from '@adonisjs/core/services/app'
 import type { Config } from '@japa/runner/types'
-import { pluginAdonisJS } from '@japa/plugin-adonisjs'
 import testUtils from '@adonisjs/core/services/test_utils'
 
-/**
- * This file is imported by the "bin/test.ts" entrypoint file
- */
+export const plugins: Config['plugins'] = [assert(), apiClient()]
 
-/**
- * Configure Japa plugins in the plugins array.
- * Learn more - https://japa.dev/docs/runner-config#plugins-optional
- */
-export const plugins: Config['plugins'] = [assert(), apiClient(), pluginAdonisJS(app)]
+export const reporters: Config['reporters'] = []
 
-/**
- * Configure lifecycle function to run before and after all the
- * tests.
- *
- * The setup functions are executed before all the tests
- * The teardown functions are executer after all the tests
- */
 export const runnerHooks: Required<Pick<Config, 'setup' | 'teardown'>> = {
-  setup: [],
+  setup: [() => TestUtils.ace().loadCommands()],
   teardown: [],
 }
 
-/**
- * Configure suites by tapping into the test suite instance.
- * Learn more - https://japa.dev/docs/test-suites#lifecycle-hooks
- */
 export const configureSuite: Config['configureSuite'] = (suite) => {
-  if (['browser', 'functional', 'e2e'].includes(suite.name)) {
-    return suite.setup(() => testUtils.httpServer().start())
+  if (suite.name === 'functional') {
+    suite.setup(() => TestUtils.httpServer().start())
   }
 }
--- tests/functional/functional.spec.ts
+++ tests/functional/functional.spec.ts
@@ -1,9 +1,5 @@
 import { test } from '@japa/runner'
 
-test.group('Functional', () => {
-  test('example test', async ({ assert, client }) => {
-    const response = await client.get('/moments')
-
-    console.log(response.body)
-  })
+test('example test', async ({ client }) => {
+  console.log(client.get)
 })
